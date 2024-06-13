59ad97f9b2f92ebea59bab5ee69a10f6fd9f19ef
ClaPascarella <claudiopascarella1996@gmail.com>
2024-06-13 17:50:58 +0200

    file php

--- elabora.php
+++ elabora.php
@@ -1,11 +1,9 @@
 <?php
-//Per prima cosa, recupero i dati passati nella query string
-//lettura veloce: https://it.wikipedia.org/wiki/Query_string
-//esempio: http://localhost/pagina?chiave=valore&nome=luca&cognome=lambiase
+
 $nome = $_GET["nome"] ?? "Utente";
 $cognome = $_GET["cognome"] ?? "Anonimo";
 
-//preparo la stringa in anticipo, così l'html resta più leggibile
+
 $msg = "Hello, $nome $cognome";
 ?>
 <!DOCTYPE html>
